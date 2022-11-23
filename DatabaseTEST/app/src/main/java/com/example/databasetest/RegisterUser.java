9f366b5fc4a13532a23a629d4a7150605ac68084
NathanBoj <nathan.bojczuk@ontariotechu.net>
2022-11-23 00:09:36 -0500

    !

---
+++ DatabaseTEST/app/src/main/java/com/example/databasetest/RegisterUser.java
@@ -0,0 +1,103 @@
+package com.example.databasetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterUser extends AppCompatActivity {
+
+    private EditText name_reg, email_reg, age_reg, password_reg;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        //Initialize views
+        Button but_button_reg = findViewById(R.id.registerUser);
+        name_reg = findViewById(R.id.fullName);
+        email_reg = findViewById(R.id.emailRegister);
+        age_reg = findViewById(R.id.age);
+        password_reg = findViewById(R.id.passwordRegister);
+
+        //Register the user
+        but_button_reg.setOnClickListener(view -> registerUser());
+    }
+
+    private void registerUser() {
+        String name = name_reg.getText().toString();
+        String email = email_reg.getText().toString();
+        String age = age_reg.getText().toString();
+        String password = password_reg.getText().toString();
+
+        //Input Validation
+        if (name.isEmpty()){
+            name_reg.setError("Name is required!");
+            name_reg.requestFocus();
+            return;
+        }
+        if (email.isEmpty()){
+            email_reg.setError("Email is required!");
+            email_reg.requestFocus();
+            return;
+        }
+        if (age.isEmpty()){
+            age_reg.setError("Age is required!");
+            age_reg.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            password_reg.setError("Password is required!");
+            password_reg.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            email_reg.setError("Provide valid Email!");
+            email_reg.requestFocus();
+            return;
+        }
+        if (password.length() < 6){
+            password_reg.setError("Password must be at least 6 characters!");
+            password_reg.requestFocus();
+            return;
+        }
+
+        //Create user in FireBase
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterUser.this, task -> {
+                    if (task.isSuccessful()) {
+
+                        //Add user attributes to realtime database
+                        User user = new User(name,email, Integer.parseInt(age));
+
+                        FirebaseDatabase.getInstance().getReference("Users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user);
+
+                        Toast.makeText(RegisterUser.this, "Registration Success!", Toast.LENGTH_SHORT).show();
+
+                        //Send user back to login page
+                        startActivity(new Intent(RegisterUser.this, MainActivity.class));
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(RegisterUser.this, "Error in Registration!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+
+    }
+}
---
+++ DatabaseTEST/app/src/main/java/com/example/databasetest/User.java
@@ -0,0 +1,19 @@
+package com.example.databasetest;
+
+public class User {
+
+    public String Name, email;
+    public int age;
+
+    public User(){
+
+    }
+
+    public User(String Name, String email, int age){
+        this.Name = Name;
+        this.email = email;
+        this.age = age;
+    }
+
+}
+
